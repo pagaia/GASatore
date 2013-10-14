6d11da547cba98f68fee3b49883932ec2e25a296
Re Mo <listemailing@gmail.com>
2013-10-14 23:49:46 +0200

    file for test

---
+++ index.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once 'mysql.php';
+//require_once 'user.php';
+require_once 'KLogger.php';
+//require_once 'prodotto.php';
+require_once 'classes.php';
+
+$log = new KLogger ( "log.txt" , KLogger::DEBUG );
+// Do database work that throws an exception
+$log->LogError("An exception was thrown in ThisFunction()");
+$time_ms= date("yyy"); 
+// Print out some information
+$log->LogInfo("Internal Query Time: $time_ms milliseconds");
+ 
+// Print out the value of some variables
+
+
+// start database connection
+$db = new Database('localhost', 'gaabe', 'gaabe', 'gasatore');
+
+$user= new User($db);
+//$user->newUser($db,'username', 'password', 'tess', 'surnae', 'tel', 'mobile', 'email', 'status');
+$user->newUser('username', 'password', 'Cristina', 'Ananasso', 'tel', 'mobile', 'cri.ananasso@hotmail.it', 'second email', 'address', 1001,0,1,1,2);
+$user2 = new User($db);
+$user2->newUser('username', 'password', 'Remo', 'Moro', 'tel', 'mobile', 'remo.moro@gmail.com', 'second email', 'address', 1002,0,1,1,1);
+
+
+
+//add Product category
+$pcategory = new productCategory();
+$pcategory->newCategory($db, "pane", "Tutti i prodotti di pane");
+
+$cat2 = new productCategory();
+$cat2->newCategory($db, "uova", "Tutti i tipi di uova");
+
+$p = new Product($db);
+$p->newProduct('uova da 4', 'pacchetto di 4 uova', 2, "4.10");
+
+//$user->loadInfo(null, $db);
+//echo var_dump($user);
+//$log->LogDebug("User Count: $user");
+
+/* $jsonenc = json_encode($user);
+echo "json encoded: $jsonenc\n";
+ 
+echo "json decode 1 case " . var_dump(json_decode($jsonenc, true));
+echo "json second case " . var_dump(json_decode($jsonenc));
+*/
+?>
